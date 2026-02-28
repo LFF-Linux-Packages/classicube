e86dca93136da414470f17db70074279094173b0
NoiseyRobot <cthqco-ct@outlook.com>
2026-02-27 20:07:57 -0500

    go back

--- install.py
+++ install.py
@@ -34,8 +34,18 @@ Comment=Minecraft Classic inspired sandbox game
 Name=ClassiCube
 Exec={GAME_DIR}/ClassiCube
 Icon={ICON_FILE}
+Path={GAME_DIR}
 Terminal=false
 Categories=Game;
+Actions=singleplayer;resume;
+
+[Desktop Action singleplayer]
+Name=Start singleplayer
+Exec={GAME_DIR}/ClassiCube --singleplayer
+
+[Desktop Action resume]
+Name=Resume last server
+Exec={GAME_DIR}/ClassiCube --resume
 """
 
 # Write the desktop entry
